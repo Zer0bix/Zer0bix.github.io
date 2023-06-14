8b92594c5ed6f2be295aaa9e0c418e01581958e8
Zer0bix <106792642+Zer0bix@users.noreply.github.com>
2023-06-14 14:42:51 +1000

    Icon and fix
     - Changed the icon for the main tab to be a black hole from flaticon
     - changed the links to take the user to the new Idle_Game website, instead of the old website.

--- main.js
+++ main.js
@@ -3,9 +3,3 @@ function open_table() {
         document.getElementById("Games_Table").style.display = "block";
     else document.getElementById("Games_Table").style.display = "none";
 }
-
-
-document.getElementById("toggleVisibilityButton").addEventListener("click", function(button) {    
-    if (document.getElementById("displaytable").style.display === "none")             document.getElementById("displaytable").style.display = "block";
-    else document.getElementById("displaytable").style.display = "none";
- });
